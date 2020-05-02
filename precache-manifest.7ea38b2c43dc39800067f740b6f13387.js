51ded02d88236180b4b03b039b3456a7a53e73b3
Juan Valerian Delima <juanvaleriandelima9@gmail.com>
2020-05-03 00:04:16 +0700

    Updates

---
+++ precache-manifest.7ea38b2c43dc39800067f740b6f13387.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2710cffbe700f7065fc04ca5cb3a470f",
+    "url": "/react-page/index.html"
+  },
+  {
+    "revision": "d0b1cbc04e0952408189",
+    "url": "/react-page/static/css/main.3b08b7a4.chunk.css"
+  },
+  {
+    "revision": "2f6212c7a19eb8add4cb",
+    "url": "/react-page/static/js/2.c34e0e41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-page/static/js/2.c34e0e41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0b1cbc04e0952408189",
+    "url": "/react-page/static/js/main.61841770.chunk.js"
+  },
+  {
+    "revision": "80f7ad6ac89f33c8b175",
+    "url": "/react-page/static/js/runtime-main.f20c3adb.js"
+  },
+  {
+    "revision": "cb0ae9992fca4a8042b687f8c1098c78",
+    "url": "/react-page/static/media/github-hover.cb0ae999.png"
+  },
+  {
+    "revision": "2b2cf66b15d0f1a66c3e4050c802b9da",
+    "url": "/react-page/static/media/github.2b2cf66b.png"
+  },
+  {
+    "revision": "276d7fb8d193a8ac0f798832557ab2ab",
+    "url": "/react-page/static/media/ig-hover.276d7fb8.png"
+  },
+  {
+    "revision": "9e54efc01f14bdf6b829edd1b052356e",
+    "url": "/react-page/static/media/ig.9e54efc0.png"
+  },
+  {
+    "revision": "b763f7720e144a9af43fd4633cf957a6",
+    "url": "/react-page/static/media/photo.b763f772.jpeg"
+  }
+]);
